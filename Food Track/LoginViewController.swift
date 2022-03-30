7af55c6fc897af0b62fb57d26817037313a82f12
HernanHernandez <67992122+HernanGHernandez@users.noreply.github.com>
2022-03-29 17:51:36 -0700

    added launch, Sign-up and Login pages

---
+++ Food Track/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Food Track
+//
+//  Created by Hernan Hernandez on 3/29/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
