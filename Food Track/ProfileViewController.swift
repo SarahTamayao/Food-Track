ce9a8419546bb214779fc1316c6114900010c648
Rodrigo Andrade <randrade709@gmail.com>
2022-04-18 14:40:14 -0700

    User can logout and stays logged in

---
+++ Food Track/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Food Track
+//
+//  Created by Rodrigo Andrade on 4/18/22.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+
+}
